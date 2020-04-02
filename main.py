533a4656e25eaaa3822a4935ed019f13c7b433c1
Jixin Jia <jixin.jia@outlook.com>
2020-04-02 21:36:49 +0900

    update

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from datetime import timedelta
 
 nRows = 1000
 
+choices = ['apple','banana','orange','lemon']
+
 pdf = pd.DataFrame(
   {'id': range(nRows),
    'date': [(datetime.datetime.now() + timedelta(-1 * np.random.randint(1,365))).strftime('%Y-%m-%d') for i in range(nRows)],
@@ -14,6 +16,7 @@ pdf = pd.DataFrame(
    'customers': ["customer "+random.choice(string.ascii_letters.upper())+str(random.randint(1,1000)) for i in range(nRows)],
    'value': [round(np.random.uniform(0,1000),2) for i in range(nRows)],
    'products': ["product "+random.choice(string.ascii_letters.upper()) for i in range(nRows)],
+   'choices': [random.choice(choices) for i in range(nRows)],
    'discount_rate': [np.random.random() for i in range(nRows)]  
   }
 )
